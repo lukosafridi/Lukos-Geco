bcb36b89ca455d3207e62e4b0333a61db6a665fc
Muhammad Shahzaib Afridi <124906464+lukosafridi@users.noreply.github.com>
2024-03-05 03:00:22 +0500

    released games section start

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import Header from "./components/header/Header"
 import { BrowserRouter, Route, Router } from "react-router-dom"
 import Hero from "./components/hero/Hero"
+import Releasedgames from "./components/releasedgames/Releasedgames"
 
 function App() {
 
@@ -10,12 +11,15 @@ function App() {
     <>
       <div>
         <BrowserRouter>
-        <div className="head-div">
-          <Header />
-        </div>
-        
+          <div className="head-div">
+            <Header />
+          </div>
+          <div className="hero-app">
           <Hero />
-        
+          </div>
+          <div className="released-games-main">
+            <Releasedgames />
+          </div>
 
         </BrowserRouter>
       </div>
---
+++ src/components/releasedgames/Releasedgames.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './releasedgames.scss'
+
+const Releasedgames = () => {
+    return (
+        <div className='releasedgames-main'>
+            <div className="releasedgames-text-container">
+                <h2>RELEASED <span>GAMES</span></h2>
+                <p>Compete with 100 players on a remote island for winner takes showdown known issue where certain skin strategic</p>
+            </div>
+            <div className="released-games-slider">
+
+            </div>
+        </div>
+    )
+}
+
+export default Releasedgames
